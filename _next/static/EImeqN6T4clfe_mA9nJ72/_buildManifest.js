94980ad5d23bfe9afb01b806da80150e385701af
Yoshi-program <Yoshi-program@users.noreply.github.com>
2022-09-29 16:32:07 +0000

    deploy: 9752af09746bfd7885717474552ff181719ef4c0

--- _next/static/EImeqN6T4clfe_mA9nJ72/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/821-57019146b8b7f9c2.js","static/chunks/pages/index-3e7c1ead50f44700.js"],"/_error":["static/chunks/pages/_error-29b3d7bb596dbd7b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
