9bc7cdf2eae8b9f00b290eaeb582f48833fe9b80
konjikun <konjikun@users.noreply.github.com>
2022-09-30 13:24:04 +0000

    deploy: 5f9b4e801ec4569b8458bec7919cbfdbf117224a

---
+++ _next/static/CZpd3QP57HOwTeG_IZdX2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/858-a61dac3fa1535030.js","static/chunks/pages/index-e0509608324427db.js"],"/_error":["static/chunks/pages/_error-29b3d7bb596dbd7b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
