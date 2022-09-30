311a57b0ae6b3dec907738f0673a7e287a254d73
konjikun <konjikun@users.noreply.github.com>
2022-09-30 13:00:24 +0000

    deploy: 5f7092d5f6b40378797a13bbe8f26eaa8d1e514f

---
+++ _next/static/4BC_zt-CsrzhuiABpfapU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/858-a61dac3fa1535030.js","static/chunks/pages/index-05b28575640002fe.js"],"/_error":["static/chunks/pages/_error-29b3d7bb596dbd7b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/9GsaxxYdw1lVuSr2Wzu6S/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/858-a61dac3fa1535030.js","static/chunks/pages/index-4b0c5aa070129c75.js"],"/_error":["static/chunks/pages/_error-29b3d7bb596dbd7b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
